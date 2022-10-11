1b18629d107251513454cca1ef0dcff38267b1d5
ndurivin <ndurivo@gmail.com>
2022-10-11 22:09:19 +0300

    Adds Review and Genre CRUD functionalities in the routes and controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,60 @@
 class ReviewsController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :create_error
+  
+  # GET /reviews
+    def index
+      reviews = Review.all
+      render json: reviews
+    end
+  
+    # create reviews
+    def create
+      review = Review.create!(create_review_params)
+      render json: review, status: 201
+    end
+
+    # get/reviews/id
+    def show
+        review = find_review
+        render json: review
+    end
+
+    # update reviews
+    def update
+        review = find_review
+        review.update!(review_params)
+        render json: review
+    end
+
+    # delete reviews
+    def destroy
+        review = find_review
+        review.destroy
+        head :no_content
+    end
+    
+  
+    private 
+    
+    def review_params
+      params.permit(:title, :description, :rating)
+    end
+  
+    def create_review_params
+        params.permit(:title, :description, :rating, :user_id, :book_id)
+    end
+
+    def find_reviews
+      Review.find(params[:id])
+    end
+    
+    def render_not_found_response
+      render json: { error: "reviews not found" }, status: 404
+    end
+  
+    def create_error
+      render json: { error:  ["validation errors"]}, status: 422
+    end
 end
