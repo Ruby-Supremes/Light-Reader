9da3bfb45d45beae49051f45fece29dd2b3925bd
jbdura <bidurajohnson@gmail.com>
2022-10-13 21:10:40 +0300

    First client try

---
+++ client/src/components/Bookcard.js
@@ -0,0 +1,29 @@
+// import React from 'react';
+// // import { Link } from "react-router-dom";
+// import Card from 'react-bootstrap/Card'
+// import Button from 'react-bootstrap/Button';
+// import Container from 'react-bootstrap/Container';
+
+
+
+// function Bookcard({title, image, description}) {
+
+//     return (
+//         <Container>
+//             <Card style={{ width: '15rem' }}>
+//                 <Card.Img variant={image} src="holder.js/100px180" />
+//                 <Card.Body>
+//                     <Card.Title>{title}</Card.Title>
+//                     <Card.Text>
+//                     {description}
+//                     Some quick example text to build on the card title and make up the
+//                     bulk of the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Read more...</Button>
+//                 </Card.Body>
+//             </Card>
+//         </Container>
+//     )
+// }
+
+// export default Bookcard
