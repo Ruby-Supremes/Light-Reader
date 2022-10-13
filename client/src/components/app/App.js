5a409e4aa7ca3a6946ac761563d602f38f9e3f3d
Verah Kwamboka <verahnyagaka101@gmail.com>
2022-10-14 00:41:36 +0300

    merged components

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,16 +1,51 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-// import Home from "../home/Home";
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import HomeCard from '../home/ HomeCard'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import './App.css'
+import Login from '../Login'
+import SignUp from '../SignUp'
 
 export default function App() {
   return (
-    // <div>App</div>
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<HomeCard />} />
-        </Routes>
+      <HomeCard />
+      <div className="App">
+       <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/sign-in'}>
+              Light-Reader
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    Sign up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+          {/* <div>
+            <Routes>
+              <Route path="/" element={<HomeCard />} />
+            </Routes>
+          </div> */}
+         <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </Router>
   )
